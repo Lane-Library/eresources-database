e90514827487951e332e255d96d08896f9df469f
Charles Yates <ceyates@stanford.edu>
2013-08-21 09:24:59 -0700

    add delete main class

---
+++ src/main/java/edu/stanford/irt/eresources/DBDelete.java
@@ -0,0 +1,11 @@
+package edu.stanford.irt.eresources;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class DBDelete extends DBLoader {
+
+    public static void main(final String[] args) throws SQLException, IOException {
+        DBLoader.main(new String[]{"db-delete"});
+    }
+}
