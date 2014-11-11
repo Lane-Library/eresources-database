ddd48f91d6b5ec75371baef81d4467dca4eb8147
Charles Yates <ceyates@stanford.edu>
2014-11-11 10:44:02 +0200

    optimize sql

--- src/main/java/edu/stanford/irt/eresources/UpdatePrintBibInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/UpdatePrintBibInputStream.java
@@ -1,9 +1,32 @@
 package edu.stanford.irt.eresources;
 
-public class PrintBibInputStream extends BibInputStream {
+public class UpdatePrintBibInputStream extends BibInputStream {
 
     private static final String QUERY =
-            "WITH PRINT_BIBS AS "
+            "WITH updated_bibs AS "
+          + "  (SELECT bib_id "
+          + "  FROM lmldb.bib_master "
+          + "  WHERE update_date > ? "
+          + "  OR create_date    > ? "
+          + "  UNION "
+          + "  SELECT bib_mfhd.bib_id "
+          + "  FROM lmldb.bib_mfhd, "
+          + "    lmldb.mfhd_master "
+          + "  WHERE mfhd_master.mfhd_id = bib_mfhd.mfhd_id "
+          + "  AND (update_date          > ? "
+          + "  OR create_date            > ?) "
+          + "  UNION "
+          + "  SELECT bib_item.bib_id "
+          + "  FROM lmldb.bib_item, "
+          + "    lmldb.item_status item_status_1 "
+          + "  LEFT OUTER JOIN lmldb.item_status item_status_2 "
+          + "  ON (item_status_1.item_id          = item_status_2.item_id "
+          + "  AND item_status_1.item_status_date < item_status_2.item_status_date) "
+          + "  WHERE item_status_2.item_id       IS NULL "
+          + "  AND bib_item.item_id               = item_status_1.item_id "
+          + "  AND item_status_1.item_status_date > ? "
+          + "  ) "
+          + ", PRINT_BIBS AS "
           + "  (SELECT BIB_ID "
           + "  FROM LMLDB.BIB_INDEX "
           + "  WHERE INDEX_CODE = '2450' "
@@ -44,11 +67,13 @@ public class PrintBibInputStream extends BibInputStream {
           + "  ) "
           + "SELECT BIB_MFHD.BIB_ID , "
           + "  BIB_MFHD.MFHD_ID "
-          + "FROM PRINT_BIBS, "
+          + "FROM updated_bibs, "
+          + "  PRINT_BIBS, "
           + "  LMLDB.BIB_MASTER, "
           + "  LMLDB.BIB_MFHD, "
           + "  LMLDB.MFHD_MASTER "
           + "WHERE PRINT_BIBS.BIB_ID                = LMLDB.BIB_MASTER.BIB_ID "
+          + "AND updated_bibs.bib_id                = LMLDB.BIB_MASTER.BIB_ID "
           + "AND BIB_MASTER.SUPPRESS_IN_OPAC  != 'Y' "
           + "AND MFHD_MASTER.SUPPRESS_IN_OPAC != 'Y' "
           + "AND BIB_MFHD.BIB_ID               = BIB_MASTER.BIB_ID "
@@ -57,5 +82,9 @@ public class PrintBibInputStream extends BibInputStream {
     @Override
     protected String getSelectIDListSQL() {
         return QUERY;
+    }
+    
+    public static final void main(String[] args) {
+        System.out.println(QUERY);
     }
 }
