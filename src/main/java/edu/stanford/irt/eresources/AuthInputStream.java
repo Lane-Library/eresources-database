f59c34da97ae6ad649948c40ea84f17d01b8dde0
Charles Yates <ceyates@stanford.edu>
2015-01-09 13:58:48 +0200

    remove duplications

--- src/main/java/edu/stanford/irt/eresources/AuthInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/AuthInputStream.java
@@ -11,9 +11,7 @@ public class AuthInputStream extends EresourceInputStream {
     }
 
     private static final String LIST_QUERY = "SELECT BIB_MASTER.BIB_ID FROM CIFDB.BIB_INDEX, CIFDB.BIB_MASTER "
-            + " WHERE BIB_MASTER.BIB_ID = BIB_INDEX.BIB_ID AND NORMAL_HEADING = 'LANECONNEX' AND INDEX_CODE = '655H'"
-            + " AND (BIB_MASTER.UPDATE_DATE > ?"
-            + " OR BIB_MASTER.CREATE_DATE > ?)";
+            + " WHERE BIB_MASTER.BIB_ID = BIB_INDEX.BIB_ID AND NORMAL_HEADING = 'LANECONNEX' AND INDEX_CODE = '655H'";
 
     private static final String MFHD_QUERY = "SELECT NULL FROM DUAL";
 
--- src/main/java/edu/stanford/irt/eresources/UpdateAuthInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/UpdateAuthInputStream.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executor;
 
 import javax.sql.DataSource;
 
-public class UpdateAuthInputStream extends EresourceInputStream {
+public class UpdateAuthInputStream extends AuthInputStream {
 
     public UpdateAuthInputStream(DataSource dataSource, Executor executor) {
         super(dataSource, executor);
@@ -14,20 +14,6 @@ public class UpdateAuthInputStream extends EresourceInputStream {
             + " WHERE BIB_MASTER.BIB_ID = BIB_INDEX.BIB_ID AND NORMAL_HEADING = 'LANECONNEX' AND INDEX_CODE = '655H'"
             + " AND (BIB_MASTER.UPDATE_DATE > ?"
             + " OR BIB_MASTER.CREATE_DATE > ?)";
-
-    private static final String MFHD_QUERY = "SELECT NULL FROM DUAL";
-
-    private static final String RECORD_QUERY = "SELECT SEQNUM, RECORD_SEGMENT FROM CIFDB.BIB_DATA WHERE BIB_ID = ? ORDER BY SEQNUM";
-
-    @Override
-    protected String getBibQuery() {
-        return RECORD_QUERY;
-    }
-
-    @Override
-    protected String getMfhdQuery() {
-        return MFHD_QUERY;
-    }
 
     @Override
     protected String getSelectIDListSQL() {
