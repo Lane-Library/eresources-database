13bce106b00cffa9dce85c0b947277783688bc8c
Charles Yates <ceyates@stanford.edu>
2015-02-06 16:38:55 +0200

    correct SQL query

--- src/main/java/edu/stanford/irt/eresources/PrintBibInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/PrintBibInputStream.java
@@ -5,15 +5,14 @@ public class PrintBibInputStream extends BibInputStream {
     private static final String QUERY =
             "SELECT BIB_MFHD.BIB_ID , "
           + "  BIB_MFHD.MFHD_ID "
-          + "FROM "
-          + "  LMLDB.BIB_MASTER, "
+          + "FROM LMLDB.BIB_MASTER, "
           + "  LMLDB.BIB_MFHD, "
           + "  LMLDB.MFHD_MASTER "
           + "WHERE BIB_MASTER.SUPPRESS_IN_OPAC  != 'Y' "
           + "AND MFHD_MASTER.SUPPRESS_IN_OPAC != 'Y' "
           + "AND BIB_MFHD.BIB_ID               = BIB_MASTER.BIB_ID "
-          + "AND BIB_MFHD.MFHD_ID              = MFHD_MASTER.MFHD_ID"
-          + " AND BIB_MFHD.BIB_ID IN "
+          + "AND BIB_MFHD.MFHD_ID              = MFHD_MASTER.MFHD_ID "
+          + "AND BIB_MFHD.BIB_ID IN "
           + "  (SELECT BIB_ID "
           + "  FROM LMLDB.BIB_INDEX "
           + "  WHERE INDEX_CODE = '2450' "
@@ -56,9 +55,5 @@ public class PrintBibInputStream extends BibInputStream {
     @Override
     protected String getSelectIDListSQL() {
         return QUERY;
-    }
-    
-    public static void main(String[] args) {
-        System.out.println(QUERY);
     }
 }
