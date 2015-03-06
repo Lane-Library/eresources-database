d99c6499b8f4dbba656fbbc0e4d6fbdbbd91de8b
Charles Yates <ceyates@stanford.edu>
2015-03-06 07:54:42 +0200

    reformat sql

--- src/main/java/edu/stanford/irt/eresources/marc/UpdateAuthInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/marc/UpdateAuthInputStream.java
@@ -10,7 +10,8 @@ public class UpdateAuthInputStream extends UpdateEresourceInputStream {
 
     private static final String LIST_QUERY = "SELECT BIB_MASTER.BIB_ID FROM CIFDB.BIB_INDEX, CIFDB.BIB_MASTER "
             + " WHERE BIB_MASTER.BIB_ID = BIB_INDEX.BIB_ID AND NORMAL_HEADING = 'LANECONNEX' AND INDEX_CODE = '655H'"
-            + " AND (BIB_MASTER.UPDATE_DATE > ?" + " OR BIB_MASTER.CREATE_DATE > ?)";
+            + " AND (BIB_MASTER.UPDATE_DATE > ?"
+            + " OR BIB_MASTER.CREATE_DATE > ?)";
 
     private static final String MFHD_QUERY = "SELECT NULL FROM DUAL";
 
