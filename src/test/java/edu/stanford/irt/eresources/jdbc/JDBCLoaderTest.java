b20fc695c57b08e5eb28a2ded24d81fbe54b2d8e
Charles Yates <ceyates@stanford.edu>
2015-03-06 16:49:41 +0200

    add unit tests

--- src/test/java/edu/stanford/irt/eresources/jdbc/JDBCLoaderTest.java
+++ src/test/java/edu/stanford/irt/eresources/jdbc/JDBCLoaderTest.java
@@ -5,12 +5,19 @@ import static org.easymock.EasyMock.expect;
 import static org.easymock.EasyMock.isA;
 import static org.easymock.EasyMock.replay;
 import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
 
+import java.io.StringWriter;
+import java.sql.CallableStatement;
+import java.sql.Clob;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -18,11 +25,14 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.stanford.irt.eresources.Eresource;
-import edu.stanford.irt.eresources.Link;
 import edu.stanford.irt.eresources.StartDate;
 import edu.stanford.irt.eresources.Version;
 
 public class JDBCLoaderTest {
+
+    private CallableStatement callable;
+
+    private Clob clob;
 
     private Connection connection;
 
@@ -30,11 +40,11 @@ public class JDBCLoaderTest {
 
     private Eresource eresource;
 
-    private Link link;
-
     private JDBCLoader loader;
 
     private PreparedStatement pStmnt;
+
+    private ResultSet resultSet;
 
     private StartDate startDate;
 
@@ -52,10 +62,12 @@ public class JDBCLoaderTest {
         this.loader = new JDBCLoader(this.dataSource, this.translator, this.startDate);
         this.eresource = createMock(Eresource.class);
         this.version = createMock(Version.class);
-        this.link = createMock(Link.class);
         this.connection = createMock(Connection.class);
         this.stmt = createMock(Statement.class);
         this.pStmnt = createMock(PreparedStatement.class);
+        this.resultSet = createMock(ResultSet.class);
+        this.clob = createMock(Clob.class);
+        this.callable = createMock(CallableStatement.class);
     }
 
     @Test
@@ -64,23 +76,92 @@ public class JDBCLoaderTest {
         expect(this.connection.createStatement()).andReturn(this.stmt);
         expect(this.connection.prepareStatement(isA(String.class))).andReturn(this.pStmnt).times(2);
         this.connection.setAutoCommit(false);
-        // expect(this.queue.isEmpty()).andReturn(false);
-        expect(this.translator.getInsertSQL(this.eresource)).andReturn(Collections.<String> emptyList());
+        List<String> sql = new ArrayList<String>();
+        sql.add("eresource");
+        expect(this.translator.getInsertSQL(this.eresource)).andReturn(sql);
+        this.stmt.addBatch("eresource");
         expect(this.stmt.executeBatch()).andReturn(null);
-        // expect(this.queue.isEmpty()).andReturn(true);
         this.connection.commit();
         this.pStmnt.close();
         this.pStmnt.close();
         this.stmt.close();
         this.connection.close();
-        // expect(this.eresource.getVersions()).andReturn(Collections.singleton(this.version));
-        // expect(this.version.getLinks()).andReturn(Collections.singletonList(this.link));
-        // expect(this.queue.add(this.eresource)).andReturn(true);
         replay(this.pStmnt, this.stmt, this.connection, this.eresource, this.version, this.dataSource, this.translator);
         this.loader.preProcess();
         this.loader.load(Collections.singletonList(this.eresource));
         this.loader.postProcess();
-        // assertEquals(1, this.loader.getCount());
         verify(this.pStmnt, this.stmt, this.connection, this.eresource, this.version, this.dataSource, this.translator);
+    }
+
+    @Test
+    public void testHandleEresourceTEXT() throws SQLException {
+        expect(this.dataSource.getConnection()).andReturn(this.connection);
+        expect(this.connection.createStatement()).andReturn(this.stmt);
+        expect(this.connection.prepareStatement(isA(String.class))).andReturn(this.pStmnt).times(2);
+        this.connection.setAutoCommit(false);
+        List<String> sql = new ArrayList<String>();
+        sql.add("TEXT:text");
+        expect(this.translator.getInsertSQL(this.eresource)).andReturn(sql);
+        ;
+        expect(this.stmt.executeBatch()).andReturn(null);
+        expect(this.stmt.executeQuery("SELECT ERESOURCE_ID_SEQ.CURRVAL FROM DUAL")).andReturn(this.resultSet);
+        expect(this.resultSet.next()).andReturn(true);
+        expect(this.resultSet.getString(1)).andReturn("12");
+        this.pStmnt.setString(1, "12");
+        this.resultSet.close();
+        expect(this.pStmnt.executeQuery()).andReturn(this.resultSet);
+        expect(this.resultSet.next()).andReturn(true);
+        expect(this.resultSet.getClob(1)).andReturn(this.clob);
+        StringWriter writer = new StringWriter();
+        expect(this.clob.setCharacterStream(1)).andReturn(writer);
+        this.resultSet.close();
+        this.connection.commit();
+        this.pStmnt.close();
+        this.pStmnt.close();
+        this.stmt.close();
+        this.connection.close();
+        replay(this.clob, this.resultSet, this.pStmnt, this.stmt, this.connection, this.eresource, this.version,
+                this.dataSource, this.translator);
+        this.loader.preProcess();
+        this.loader.load(Collections.singletonList(this.eresource));
+        this.loader.postProcess();
+        assertEquals("text", writer.toString());
+        verify(this.clob, this.resultSet, this.pStmnt, this.stmt, this.connection, this.eresource, this.version,
+                this.dataSource, this.translator);
+    }
+
+    @Test
+    public void testPostProcess() throws SQLException {
+        expect(this.dataSource.getConnection()).andReturn(this.connection);
+        this.connection.setAutoCommit(false);
+        expect(this.connection.createStatement()).andReturn(this.stmt);
+        expect(this.connection.prepareStatement(isA(String.class))).andReturn(this.pStmnt).times(2);
+        expect(this.connection.prepareCall("call")).andReturn(this.callable);
+        expect(this.callable.execute()).andReturn(true);
+        this.callable.close();
+        this.pStmnt.close();
+        this.pStmnt.close();
+        this.stmt.close();
+        this.connection.commit();
+        this.connection.close();
+        replay(this.callable, this.dataSource, this.connection, this.stmt, this.pStmnt);
+        this.loader.setCallStatements(Collections.singletonList("call"));
+        this.loader.preProcess();
+        this.loader.count++;
+        this.loader.postProcess();
+        verify(this.callable, this.dataSource, this.connection, this.stmt, this.pStmnt);
+    }
+
+    @Test
+    public void testPreProcess() throws SQLException {
+        expect(this.dataSource.getConnection()).andReturn(this.connection);
+        this.connection.setAutoCommit(false);
+        expect(this.connection.createStatement()).andReturn(this.stmt);
+        expect(this.connection.prepareStatement(isA(String.class))).andReturn(this.pStmnt).times(2);
+        expect(this.stmt.execute("create")).andReturn(true);
+        replay(this.dataSource, this.connection, this.stmt, this.pStmnt);
+        this.loader.setCreateStatements(Collections.singletonList("create"));
+        this.loader.preProcess();
+        verify(this.dataSource, this.connection, this.stmt, this.pStmnt);
     }
 }
