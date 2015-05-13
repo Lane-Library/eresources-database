c9e682162a8f0134f9140d72cee63596d70e94cc
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-05-12 22:16:49 -0700

    only include auths with links

--- src/main/java/edu/stanford/irt/eresources/AuthInputStream.java
+++ src/main/java/edu/stanford/irt/eresources/AuthInputStream.java
@@ -4,6 +4,7 @@ public class AuthInputStream extends EresourceInputStream {
 
     private static final String LIST_QUERY = "SELECT BIB_MASTER.BIB_ID FROM CIFDB.BIB_INDEX, CIFDB.BIB_MASTER "
             + " WHERE BIB_MASTER.BIB_ID = BIB_INDEX.BIB_ID AND NORMAL_HEADING = 'LANECONNEX' AND INDEX_CODE = '655H'"
+            + " AND BIB_MASTER.BIB_ID IN (SELECT RECORD_ID AS BIB_ID from CIFDB.ELINK_INDEX WHERE ELINK_INDEX.RECORD_TYPE = 'B')"
             + " AND (BIB_MASTER.UPDATE_DATE > ?"
             + " OR BIB_MASTER.CREATE_DATE > ?)";
 
