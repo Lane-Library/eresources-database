7536e9cf2ce421851452a2d902bce163d8697c0b
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-06-24 16:37:42 -0700

    don't add primaryType to types

--- src/main/java/edu/stanford/irt/eresources/sax/SAXEresource.java
+++ src/main/java/edu/stanford/irt/eresources/sax/SAXEresource.java
@@ -397,7 +397,7 @@ public class SAXEresource implements Cloneable, Eresource {
      */
     @Override
     public Collection<String> getTypes() {
-        this.types.add(getPrimaryType());
+        // this.types.add(getPrimaryType());
         // this.types.add(WHITESPACE.matcher(getPrimaryType()).replaceAll(""));
         if (getPrimaryType().startsWith("Journal")) {
             this.types.add("Journal");
