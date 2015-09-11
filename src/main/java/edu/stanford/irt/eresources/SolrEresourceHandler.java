07551c7b22c91c3e9a945eac090fd23073884daf
Ryan Max Steinberg <ryanmax@stanford.edu>
2015-09-10 17:33:55 -0700

    handle case 110340 (don't show MeSH checktags in search) in laneweb instead of using separate meshNoCT field

--- src/main/java/edu/stanford/irt/eresources/SolrEresourceHandler.java
+++ src/main/java/edu/stanford/irt/eresources/SolrEresourceHandler.java
@@ -152,9 +152,6 @@ public class SolrEresourceHandler implements EresourceHandler {
         for (String mesh : eresource.getMeshTerms()) {
             doc.addField("mesh", mesh);
             doc.addField("mesh_parents", this.meshManager.getParentHeadings(mesh));
-            if (!this.meshManager.isChecktag(mesh) && !"nomesh".equals(mesh)) {
-                doc.addField("meshNoCT", mesh);
-            }
         }
         for (String type : eresource.getTypes()) {
             doc.addField("type", type);
