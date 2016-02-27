d4f4ad2e1f26e21f6c0bff05faeb9b08534d0020
Ryan Max Steinberg <ryanmax@stanford.edu>
2016-02-26 20:30:56 -0800

    simplify with try-with-resources

--- src/main/java/edu/stanford/irt/eresources/LanguageMap.java
+++ src/main/java/edu/stanford/irt/eresources/LanguageMap.java
@@ -3,6 +3,7 @@ package edu.stanford.irt.eresources;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class LanguageMap {
@@ -12,24 +13,14 @@ public class LanguageMap {
     private final Properties properties = new Properties();
 
     public LanguageMap() {
-        FileInputStream fis = null;
-        try {
-            File file = new File(FILENAME);
-            if (!file.exists()) {
-                throw new EresourceDatabaseException("missing " + FILENAME);
-            }
-            fis = new FileInputStream(file);
-            this.properties.load(fis);
+        File file = new File(FILENAME);
+        if (!file.exists()) {
+            throw new EresourceDatabaseException("missing " + FILENAME);
+        }
+        try (InputStream in = new FileInputStream(file)) {
+            this.properties.load(in);
         } catch (IOException e) {
             throw new EresourceDatabaseException(e);
-        } finally {
-            try {
-                if (null != fis) {
-                    fis.close();
-                }
-            } catch (IOException e) {
-                throw new EresourceDatabaseException(e);
-            }
         }
     }
 
