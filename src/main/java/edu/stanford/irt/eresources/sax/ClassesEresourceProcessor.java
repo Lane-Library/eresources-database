831c4b0476753585c56a075cc33f66fe4cf5b3f1
alainb <alainb@stanford.edu>
2016-04-27 09:40:23 -0700

    cleanup

--- src/main/java/edu/stanford/irt/eresources/sax/ClassesEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/sax/ClassesEresourceProcessor.java
@@ -1,15 +1,11 @@
 package edu.stanford.irt.eresources.sax;
 
-import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.sax.SAXResult;
@@ -18,7 +14,6 @@ import org.w3c.dom.Document;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 import org.xml.sax.helpers.DefaultHandler;
 
