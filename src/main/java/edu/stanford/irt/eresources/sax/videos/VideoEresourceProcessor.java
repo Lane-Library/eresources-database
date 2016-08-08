a03eb9d24184414a49769e9cc1e2fd7492f60927
Ryan Max Steinberg <ryanmax@stanford.edu>
2016-08-08 10:38:26 -0700

    a few sonar fixes

--- src/main/java/edu/stanford/irt/eresources/sax/videos/VideoEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/sax/videos/VideoEresourceProcessor.java
@@ -1,6 +1,5 @@
 package edu.stanford.irt.eresources.sax.videos;
 
-import java.io.IOException;
 import java.util.List;
 
 import javax.xml.transform.TransformerFactory;
@@ -27,6 +26,9 @@ public class VideoEresourceProcessor extends AbstractEresourceProcessor {
 
     protected static final String ERESOURCES = "eresources";
 
+    private static final Header USER_AGENT = new BasicHeader("User-Agent",
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:34.0) Gecko/20100101 Firefox/34.");
+
     protected ContentHandler contentHandler;
 
     protected TransformerFactory tf = TransformerFactory.newInstance();
@@ -34,9 +36,6 @@ public class VideoEresourceProcessor extends AbstractEresourceProcessor {
     protected List<String> URLs;
 
     HttpClient httpClient = null;
-
-    Header USER_AGENT = new BasicHeader("User-Agent",
-            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:34.0) Gecko/20100101 Firefox/34.");
 
     public VideoEresourceProcessor() {
         this.httpClient = HttpClients.createDefault();
@@ -67,15 +66,13 @@ public class VideoEresourceProcessor extends AbstractEresourceProcessor {
     }
 
     protected Document getDocument(final String url) {
-        CloseableHttpResponse res = null;
-        try {
-            HTMLConfiguration config = new HTMLConfiguration();
-            config.setFeature("http://xml.org/sax/features/namespaces", false);
-            config.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
-            config.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
-            HttpGet get = new HttpGet(url);
-            get.addHeader(this.USER_AGENT);
-            res = (CloseableHttpResponse) this.httpClient.execute(get);
+        HTMLConfiguration config = new HTMLConfiguration();
+        config.setFeature("http://xml.org/sax/features/namespaces", false);
+        config.setProperty("http://cyberneko.org/html/properties/default-encoding", "UTF-8");
+        config.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
+        HttpGet get = new HttpGet(url);
+        get.addHeader(USER_AGENT);
+        try (CloseableHttpResponse res = (CloseableHttpResponse) this.httpClient.execute(get)) {
             InputSource source = new InputSource(res.getEntity().getContent());
             source.setEncoding("UTF-8");
             DOMParser parser = new DOMParser(config);
@@ -83,14 +80,6 @@ public class VideoEresourceProcessor extends AbstractEresourceProcessor {
             return parser.getDocument();
         } catch (Exception e) {
             throw new EresourceDatabaseException(e);
-        } finally {
-            try {
-                if (res != null) {
-                    res.close();
-                }
-            } catch (IOException e) {
-                throw new EresourceDatabaseException(e);
-            }
         }
     }
 }
