00b4bccb09f9a9a44b2490986691b1488f6c1f1d
Ryan Max Steinberg <ryanmax@stanford.edu>
2016-11-23 11:30:57 -0800

    code cleanup

--- src/main/java/edu/stanford/irt/eresources/sax/videos/JsonVideoEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/sax/videos/JsonVideoEresourceProcessor.java
@@ -16,20 +16,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import edu.stanford.irt.eresources.EresourceDatabaseException;
 
-public abstract class JsonVideoEresourceProcessor extends AbstractVideoEresourceProcessor{
-    
- 
+public abstract class JsonVideoEresourceProcessor extends AbstractVideoEresourceProcessor {
+
     protected HttpClient httpClient = null;
 
-    private Header USER_AGENT = new BasicHeader("User-Agent",    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:34.0) Gecko/20100101 Firefox/34.");
+    private Header USER_AGENT = new BasicHeader("User-Agent",
+            "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:34.0) Gecko/20100101 Firefox/34.");
 
-    
-    
     public JsonVideoEresourceProcessor() {
         this.httpClient = HttpClients.createDefault();
     }
 
-    protected JsonNode getJsonNode(String url) throws IOException {
+    protected JsonNode getJsonNode(final String url) throws IOException {
         CloseableHttpResponse res = null;
         try {
             HTMLConfiguration config = new HTMLConfiguration();
@@ -37,9 +35,8 @@ public abstract class JsonVideoEresourceProcessor extends AbstractVideoEresource
             config.setProperty("http://cyberneko.org/html/properties/default-encoding", StandardCharsets.UTF_8.name());
             config.setProperty("http://cyberneko.org/html/properties/names/elems", "lower");
             HttpGet get = new HttpGet(url);
-            get.addHeader(USER_AGENT);
-            res = (CloseableHttpResponse) httpClient.execute(get);
-            
+            get.addHeader(this.USER_AGENT);
+            res = (CloseableHttpResponse) this.httpClient.execute(get);
             ObjectMapper mapper = new ObjectMapper();
             return mapper.readValue(res.getEntity().getContent(), JsonNode.class);
         } catch (Exception e) {
@@ -50,6 +47,4 @@ public abstract class JsonVideoEresourceProcessor extends AbstractVideoEresource
             }
         }
     }
-
-
 }
