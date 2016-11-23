49d8320edd0f7c6bb475c4efa687989a3136b82f
Ryan Max Steinberg <ryanmax@stanford.edu>
2016-11-22 21:20:17 -0800

    no need for FILENAME variable

--- src/main/java/edu/stanford/irt/eresources/LanguageMap.java
+++ src/main/java/edu/stanford/irt/eresources/LanguageMap.java
@@ -8,12 +8,10 @@ import java.util.Properties;
 
 public class LanguageMap {
 
-    private static String FILENAME = "languages.properties";
-
     private final Properties properties = new Properties();
 
     public LanguageMap() {
-        File file = new File(FILENAME);
+        File file = new File("languages.properties");
         try (InputStream in = new FileInputStream(file)) {
             this.properties.load(in);
         } catch (IOException e) {
