06e9fa2223755f3c803db7fab38293f57926eadb
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-05-16 14:19:50 -0700

    log all er db exceptions at ERROR level
    related to case 133985: all bib records missing from search and browse
    pages
    frequent oracle connection errors are going unreported

--- src/main/java/edu/stanford/irt/eresources/EresourceDatabaseException.java
+++ src/main/java/edu/stanford/irt/eresources/EresourceDatabaseException.java
@@ -1,18 +1,26 @@
 package edu.stanford.irt.eresources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class EresourceDatabaseException extends RuntimeException {
+
+    private static final Logger LOG = LoggerFactory.getLogger(EresourceDatabaseException.class);
 
     private static final long serialVersionUID = 1L;
 
     public EresourceDatabaseException(final String message) {
         super(message);
+        LOG.error(message);
     }
 
     public EresourceDatabaseException(final String message, final Throwable cause) {
         super(message, cause);
+        LOG.error(message, cause);
     }
 
     public EresourceDatabaseException(final Throwable cause) {
         super(cause);
+        LOG.error("EresourceDatabaseException", cause);
     }
 }
