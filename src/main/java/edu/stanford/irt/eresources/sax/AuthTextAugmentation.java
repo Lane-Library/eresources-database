8cd017d7a87eb714565e19de95cc25735a6c8899
Ryan Max Steinberg <ryanmax@stanford.edu>
2017-10-17 20:48:42 -0700

    sonar
    Make the "java.util.Map" class "Serializable" or don't write it.
    Make the "LOG" logger private static final and rename it to comply with
    the format "log(?:ger)?".

--- src/main/java/edu/stanford/irt/eresources/sax/AuthTextAugmentation.java
+++ src/main/java/edu/stanford/irt/eresources/sax/AuthTextAugmentation.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -16,9 +17,9 @@ import edu.stanford.irt.eresources.EresourceDatabaseException;
 
 public class AuthTextAugmentation {
 
-    protected static final Logger LOG = LoggerFactory.getLogger(AuthTextAugmentation.class);
-
     private static final String AUGMENTATION_FILE = "auth-augmentations.obj";
+
+    private static final Logger log = LoggerFactory.getLogger(AuthTextAugmentation.class);
 
     private static final int ONE_DAY = 1000 * 60 * 60 * 24;
 
@@ -29,15 +30,15 @@ public class AuthTextAugmentation {
         if (!objFile.exists() || objFile.lastModified() < System.currentTimeMillis() - ONE_DAY) {
             this.augmentations = augmentationsService.buildAugmentations();
             try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(AUGMENTATION_FILE))) {
-                oos.writeObject(this.augmentations);
+                oos.writeObject(new HashMap<>(this.augmentations));
             } catch (IOException e) {
                 throw new EresourceDatabaseException(e);
             }
         } else {
             try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(objFile))) {
-                this.augmentations = (Map<String, String>) ois.readObject();
+                this.augmentations = (HashMap<String, String>) ois.readObject();
             } catch (IOException e) {
-                LOG.error("can't open augmentations file", e);
+                log.error("can't open augmentations file", e);
                 this.augmentations = Collections.emptyMap();
             } catch (ClassNotFoundException e) {
                 throw new EresourceDatabaseException(e);
@@ -45,7 +46,6 @@ public class AuthTextAugmentation {
         }
     }
 
-    @SuppressWarnings("unchecked")
     public String getAuthAugmentations(final String controlNumber) {
         return this.augmentations.get(controlNumber);
     }
