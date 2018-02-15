503c70678952d0d25421b273df9d57d41ec354c7
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-02-14 16:31:48 -0800

    add unit tests

--- src/test/java/edu/stanford/irt/eresources/marc/AuthMarcEresourceTest.java
+++ src/test/java/edu/stanford/irt/eresources/marc/AuthMarcEresourceTest.java
@@ -12,23 +12,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import edu.stanford.lane.catalog.Record;
+import edu.stanford.lane.catalog.Record.Field;
+import edu.stanford.lane.catalog.Record.Subfield;
 
 public class AuthMarcEresourceTest {
 
     private AuthMarcEresource eresource;
 
+    private Field field;
+
     private KeywordsStrategy keywordsStrategy;
 
     private Record record;
+
+    private Subfield subfield;
 
     private TypeFactory typeFactory;
 
     @Before
     public void setUp() {
         this.record = mock(Record.class);
+        this.field = mock(Field.class);
+        this.subfield = mock(Subfield.class);
         this.keywordsStrategy = mock(KeywordsStrategy.class);
         this.typeFactory = mock(TypeFactory.class);
         this.eresource = new AuthMarcEresource(this.record, this.keywordsStrategy, this.typeFactory);
+    }
+
+    @Test
+    public void testGetItemCount() {
+        assertEquals(0, this.eresource.getItemCount()[0]);
     }
 
     @Test
@@ -53,10 +66,43 @@ public class AuthMarcEresourceTest {
     }
 
     @Test
+    public void testGetVersionsF856() {
+        expect(this.record.getFields()).andReturn(Collections.singletonList(this.field));
+        expect(this.field.getTag()).andReturn("856");
+        replay(this.record, this.field);
+        assertEquals(MarcVersion.class, this.eresource.getVersions().get(0).getClass());
+        verify(this.record, this.field);
+    }
+
+    @Test
     public void testGetYear() {
         expect(this.record.getFields()).andReturn(Collections.emptyList());
         replay(this.record);
         assertEquals(0, this.eresource.getYear());
         verify(this.record);
+    }
+
+    @Test
+    public void testGetYearContinuing() {
+        expect(this.record.getFields()).andReturn(Collections.singletonList(this.field));
+        expect(this.field.getTag()).andReturn("943");
+        expect(this.field.getSubfields()).andReturn(Collections.singletonList(this.subfield));
+        expect(this.subfield.getCode()).andReturn('b');
+        expect(this.subfield.getData()).andReturn("continuing");
+        replay(this.record, this.field, this.subfield);
+        assertEquals(BibMarcEresource.THIS_YEAR, this.eresource.getYear());
+        verify(this.record, this.field, this.subfield);
+    }
+
+    @Test
+    public void testGetYearValue() {
+        expect(this.record.getFields()).andReturn(Collections.singletonList(this.field));
+        expect(this.field.getTag()).andReturn("943");
+        expect(this.field.getSubfields()).andReturn(Collections.singletonList(this.subfield));
+        expect(this.subfield.getCode()).andReturn('b');
+        expect(this.subfield.getData()).andReturn("1900");
+        replay(this.record, this.field, this.subfield);
+        assertEquals(1900, this.eresource.getYear());
+        verify(this.record, this.field, this.subfield);
     }
 }
