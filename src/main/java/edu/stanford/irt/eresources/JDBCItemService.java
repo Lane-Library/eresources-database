a1087b46831607eedfa9027bc0ac8954b65cbbba
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-02-15 22:56:05 -0800

    magic number

--- src/main/java/edu/stanford/irt/eresources/JDBCItemService.java
+++ src/main/java/edu/stanford/irt/eresources/JDBCItemService.java
@@ -14,7 +14,7 @@ public class JDBCItemService implements ItemService {
     /*
      * this query from: http://stackoverflow.com/questions/7745609/sql-select-only-rows-with-max-value-on-a-column
      */
-    private static final String AVAILABLE_QUERY = "SELECT bi.bib_id, COUNT(*) FROM lmldb.bib_item bi, "
+    private static final String AVAILABLE_QUERY = "SELECT bi.bib_id, COUNT(*) count FROM lmldb.bib_item bi, "
             + "  lmldb.item_status item_status_1 LEFT OUTER JOIN lmldb.item_status item_status_2 "
             + "ON (item_status_1.item_id          = item_status_2.item_id "
             + "AND item_status_1.item_status_date < item_status_2.item_status_date) "
@@ -27,7 +27,7 @@ public class JDBCItemService implements ItemService {
 
     private static final int FETCH_SIZE = 100000;
 
-    private static final String TOTAL_QUERY = "SELECT bib_id, COUNT(DISTINCT item_status.item_id) "
+    private static final String TOTAL_QUERY = "SELECT bib_id, COUNT(DISTINCT item_status.item_id) count "
             + "FROM lmldb.bib_item, lmldb.item_status " + "WHERE bib_item.item_id = item_status.item_id "
             + "GROUP BY bib_id";
 
@@ -55,7 +55,7 @@ public class JDBCItemService implements ItemService {
                 ResultSet rs = statement.executeQuery(query);) {
             rs.setFetchSize(FETCH_SIZE);
             while (rs.next()) {
-                map.put(Integer.valueOf(rs.getInt(1)), Integer.valueOf(rs.getInt(2)));
+                map.put(Integer.valueOf(rs.getInt("bib_id")), Integer.valueOf(rs.getInt("count")));
             }
         } catch (SQLException e) {
             throw new EresourceDatabaseException(e);
