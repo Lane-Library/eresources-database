46620de68a7126ac81f75a5597b204f93311e0c2
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-06-12 11:44:07 -0700

    new unit test

---
+++ src/test/java/edu/stanford/irt/eresources/sax/SAXLinkTest.java
@@ -0,0 +1,47 @@
+package edu.stanford.irt.eresources.sax;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.stanford.irt.eresources.Version;
+
+public class SAXLinkTest {
+
+    @Test
+    public final void testGetLinkText() {
+        SAXLink link = new SAXLink();
+        link.setLabel("label");
+        SAXVersion version = new SAXVersion();
+        version.setSummaryHoldings("summaryHoldings");
+        version.addLink(new SAXLink());
+        link.setVersion(version);
+        assertEquals("summaryHoldings", link.getLinkText());
+        link.setLabel("");
+        link.setVersion(new SAXVersion());
+        assertTrue(link.getLinkText().isEmpty());
+    }
+
+    @Test
+    public final void testSAXLink() {
+        SAXLink link = new SAXLink();
+        link.setInstruction("instruction");
+        link.setLabel("label");
+        link.setUrl("url");
+        Version version = new SAXVersion();
+        link.setVersion(version);
+        assertEquals("instruction", link.getAdditionalText());
+        assertEquals("label", link.getLabel());
+        assertEquals("label", link.getLinkText());
+        assertEquals("url", link.getUrl());
+    }
+
+    @Test
+    public final void testSAXLinkImpactfactor() {
+        SAXLink link = new SAXLink();
+        link.setLabel("impact factor");
+        assertEquals("impact factor", link.getLabel());
+        assertEquals("Impact Factor", link.getLinkText());
+    }
+}
