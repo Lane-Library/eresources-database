6c363e646c11474f9bd1fe3aaaa1b4f2c5fb463c
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-10-30 09:22:57 -0700

    return a copy of ...

--- src/main/java/edu/stanford/irt/eresources/redivis/Dataset.java
+++ src/main/java/edu/stanford/irt/eresources/redivis/Dataset.java
@@ -1,5 +1,6 @@
 package edu.stanford.irt.eresources.redivis;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -47,7 +48,7 @@ public class Dataset {
      * @return the collections
      */
     public List<DatasetCollection> getCollections() {
-        return this.collections;
+        return new ArrayList<>(this.collections);
     }
 
     /**
@@ -61,7 +62,7 @@ public class Dataset {
      * @return the documentations
      */
     public List<Documentation> getDocumentations() {
-        return this.documentations;
+        return new ArrayList<>(this.documentations);
     }
 
     /**
@@ -96,7 +97,7 @@ public class Dataset {
      * @return the tags
      */
     public List<Tag> getTags() {
-        return this.tags;
+        return new ArrayList<>(this.tags);
     }
 
     /**
@@ -110,7 +111,7 @@ public class Dataset {
      * @return the updatedAt
      */
     public Date getUpdatedAt() {
-        return this.updatedAt;
+        return new Date(this.updatedAt.getTime());
     }
 
     /**
@@ -124,110 +125,6 @@ public class Dataset {
      * @return the variables
      */
     public List<Variable> getVariables() {
-        return this.variables;
-    }
-
-    /**
-     * @param accessLevel
-     *            the accessLevel to set
-     */
-    public void setAccessLevel(final String accessLevel) {
-        this.accessLevel = accessLevel;
-    }
-
-    /**
-     * @param collections
-     *            the collections to set
-     */
-    public void setCollections(final List<DatasetCollection> collections) {
-        this.collections = collections;
-    }
-
-    /**
-     * @param description
-     *            the description to set
-     */
-    public void setDescription(final String description) {
-        this.description = description;
-    }
-
-    /**
-     * @param documentations
-     *            the documentations to set
-     */
-    public void setDocumentations(final List<Documentation> documentations) {
-        this.documentations = documentations;
-    }
-
-    /**
-     * @param entity
-     *            the entity to set
-     */
-    public void setEntity(final Entity entity) {
-        this.entity = entity;
-    }
-
-    /**
-     * @param id
-     *            the id to set
-     */
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    /**
-     * @param name
-     *            the name to set
-     */
-    public void setName(final String name) {
-        this.name = name;
-    }
-
-    /**
-     * @param organization
-     *            the organization to set
-     */
-    public void setOrganization(final Organization organization) {
-        this.organization = organization;
-    }
-
-    /**
-     * @param tags
-     *            the tags to set
-     */
-    public void setTags(final List<Tag> tags) {
-        this.tags = tags;
-    }
-
-    /**
-     * @param temporalRange
-     *            the temporalRange to set
-     */
-    public void setTemporalRange(final TemporalRange temporalRange) {
-        this.temporalRange = temporalRange;
-    }
-
-    /**
-     * @param updatedAt
-     *            the updatedAt to set
-     */
-    public void setUpdatedAt(final Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
-    /**
-     * @param url
-     *            the url to set
-     */
-    public void setUrl(final String url) {
-        this.url = url;
-    }
-
-    /**
-     * @param variables
-     *            the variables to set
-     */
-    public void setVariables(final List<Variable> variables) {
-        this.variables = variables;
+        return new ArrayList<>(this.variables);
     }
 }
