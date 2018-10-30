89b5b737e715e23b7ec816baa1a9fde725407a82
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-10-30 12:19:50 -0700

    move maybeDeleteOldRecords method to SolrLoader

--- src/main/java/edu/stanford/irt/eresources/SolrReload.java
+++ src/main/java/edu/stanford/irt/eresources/SolrReload.java
@@ -1,11 +1,6 @@
 package edu.stanford.irt.eresources;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SolrReload extends SolrLoader {
 
@@ -13,12 +8,6 @@ public class SolrReload extends SolrLoader {
             + " recordType:class OR recordType:laneblog OR recordType:web)";
 
     private static final int EXPECTED_MIN_BIBS = 300_000;
-
-    private static final Logger LOG = LoggerFactory.getLogger(SolrReload.class);
-
-    public static void main(final String[] args) {
-        SolrLoader.main(new String[] { "solr-reload" });
-    }
 
     @Override
     public void load() {
@@ -28,22 +17,6 @@ public class SolrReload extends SolrLoader {
         // set update date to null so Processors fetch everything
         this.setUpdatedDateQuery(null);
         super.load();
-        maybeDeleteOldRecords(updateDate.format(SOLR_DATE_FIELD_FORMATTER));
-    }
-
-    private void maybeDeleteOldRecords(final String lastUpdate) {
-        int updatedRecords = super.getHandler().getCount();
-        if (updatedRecords < EXPECTED_MIN_BIBS) {
-            LOG.error("fewer updated records than expected; won't delete old records;"
-                    + " udpated records: {}; min expected: {}", updatedRecords, EXPECTED_MIN_BIBS);
-        } else {
-            try {
-                // delete everything older than lastUpdate
-                this.solrClient.deleteByQuery(BASE_QUERY + " AND updated:[* TO " + lastUpdate + "]");
-                this.solrClient.commit();
-            } catch (SolrServerException | IOException e) {
-                throw new EresourceDatabaseException(e);
-            }
-        }
+        maybeDeleteOldRecords(updateDate.format(SOLR_DATE_FIELD_FORMATTER), BASE_QUERY, EXPECTED_MIN_BIBS);
     }
 }
--- src/main/java/edu/stanford/irt/eresources/SolrUpdate.java
+++ src/main/java/edu/stanford/irt/eresources/SolrUpdate.java
@@ -2,10 +2,6 @@ package edu.stanford.irt.eresources;
 
 public class SolrUpdate extends SolrLoader {
 
-    public static void main(final String[] args) {
-        SolrLoader.main(new String[] { "solr-update" });
-    }
-
     @Override
     public void load() {
         this.setUpdatedDateQuery("NOT recordType:pubmed");
