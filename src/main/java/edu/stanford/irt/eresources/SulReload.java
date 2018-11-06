c508ec562b42f016fe7f31c87ba9887f957b084c
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-11-05 16:06:28 -0800

    use maybeDeleteOldRecords from super

--- src/main/java/edu/stanford/irt/eresources/SulReload.java
+++ src/main/java/edu/stanford/irt/eresources/SulReload.java
@@ -1,23 +1,12 @@
 package edu.stanford.irt.eresources;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
-
-import org.apache.solr.client.solrj.SolrServerException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class SulReload extends SolrLoader {
 
     private static final String BASE_QUERY = "(recordType:sul)";
 
-    private static final int EXPECTED_MIN_BIBS = 1_000_000;
-
-    private static final Logger LOG = LoggerFactory.getLogger(SulReload.class);
-
-    public static void main(final String[] args) {
-        SolrLoader.main(new String[] { "sul-reload" });
-    }
+    private static final int EXPECTED_MIN_RECORDS = 1_000_000;
 
     @Override
     public void load() {
@@ -27,22 +16,6 @@ public class SulReload extends SolrLoader {
         // set update date to null so Processors fetch everything
         this.setUpdatedDateQuery(null);
         super.load();
-        maybeDeleteOldRecords(updateDate.format(SOLR_DATE_FIELD_FORMATTER));
-    }
-
-    private void maybeDeleteOldRecords(final String lastUpdate) {
-        int updatedRecords = super.getHandler().getCount();
-        if (updatedRecords < EXPECTED_MIN_BIBS) {
-            LOG.error("fewer updated sul records than expected; won't delete old records;"
-                    + " udpated records: {}; min expected: {}", updatedRecords, EXPECTED_MIN_BIBS);
-        } else {
-            try {
-                // delete everything older than lastUpdate
-                this.solrClient.deleteByQuery(BASE_QUERY + " AND updated:[* TO " + lastUpdate + "]");
-                this.solrClient.commit();
-            } catch (SolrServerException | IOException e) {
-                throw new EresourceDatabaseException(e);
-            }
-        }
+        maybeDeleteOldRecords(updateDate.format(SOLR_DATE_FIELD_FORMATTER), BASE_QUERY, EXPECTED_MIN_RECORDS);
     }
 }
