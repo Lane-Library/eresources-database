c897c799e5dcefae32b6d8beca9a5a1a70ecb541
Ryan Max Steinberg <ryanmax@stanford.edu>
2018-11-05 16:01:26 -0800

    some sonar fixes

--- src/main/java/edu/stanford/irt/eresources/marc/SulMARCRecordEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/marc/SulMARCRecordEresourceProcessor.java
@@ -1,7 +1,9 @@
 package edu.stanford.irt.eresources.marc;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import edu.stanford.irt.eresources.AbstractEresourceProcessor;
@@ -12,6 +14,16 @@ import edu.stanford.lane.catalog.Record.Field;
 import edu.stanford.lane.catalog.RecordCollection;
 
 public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor {
+
+    private static final Pattern BEGINS_HTTPS_OR_ENDS_SLASH = Pattern.compile("(^https?://|/$)");
+
+    private static final int F008_DATES_BEGIN = 7;
+
+    private static final int F008_DATES_END = 15;
+
+    private static final Pattern NOT_ALPHANUM_OR_SPACE = Pattern.compile("[^a-zA-Z_0-9 ]");
+
+    private static final Pattern NOT_DIGIT = Pattern.compile("\\D");
 
     private List<String> acceptableDBCallNumbers;
 
@@ -35,7 +47,8 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
         this.keywordsStrategy = keywordsStrategy;
         this.recordCollectionFactory = recordCollectionFactory;
         this.typeFactory = typeFactory;
-        this.acceptableLCCallNumberPrefixes = acceptableLCCallNumberPrefixes;
+        this.acceptableLCCallNumberPrefixes = new ArrayList<>(acceptableLCCallNumberPrefixes);
+        this.acceptableDBCallNumbers = new ArrayList<>(acceptableDBCallNumbers);
         this.laneDedupAugmentation = laneDedupAugmentation;
     }
 
@@ -82,8 +95,9 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
     }
 
     private boolean isLaneDuplicate(final Record record) {
-        String catkey = MARCRecordSupport.getFields(record, "001").map(Field::getData).findFirst().orElse("0")
-                .replaceAll("\\D", "");
+        String catkey = NOT_DIGIT
+                .matcher(MARCRecordSupport.getFields(record, "001").map(Field::getData).findFirst().orElse("0"))
+                .replaceAll("");
         if (this.laneDedupAugmentation.isDuplicate(LaneDedupAugmentation.KEY_CATKEY, catkey)) {
             return true;
         }
@@ -118,16 +132,18 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
         }
         Set<String> urls = MARCRecordSupport.getSubfieldData(record, "856", "u")
                 .map((final String s) -> s.replace("https://stanford.idm.oclc.org/login?url=", "")).map(String::trim)
-                .map((final String s) -> s.replaceAll("(^https?://|/$)", "")).collect(Collectors.toSet());
+                .map((final String s) -> BEGINS_HTTPS_OR_ENDS_SLASH.matcher(s).replaceAll(""))
+                .collect(Collectors.toSet());
         for (String url : urls) {
             if (this.laneDedupAugmentation.isDuplicate(LaneDedupAugmentation.KEY_URL, url)) {
                 return true;
             }
         }
-        String title = MARCRecordSupport.getSubfieldData(record, "245", "a").findFirst().map(String::trim).orElse("")
-                .replaceAll("[^a-zA-Z_0-9 ]", "");
+        String title = NOT_ALPHANUM_OR_SPACE
+                .matcher(MARCRecordSupport.getSubfieldData(record, "245", "a").findFirst().map(String::trim).orElse(""))
+                .replaceAll("");
         String dates = MARCRecordSupport.getFields(record, "008").map(Field::getData).findFirst()
-                .map((final String s) -> s.substring(7, 15)).orElse("00000000");
+                .map((final String s) -> s.substring(F008_DATES_BEGIN, F008_DATES_END)).orElse("00000000");
         StringBuilder sb = new StringBuilder(title);
         sb.append(dates);
         return this.laneDedupAugmentation.isDuplicate(LaneDedupAugmentation.KEY_TITLE_DATE, sb.toString());
