d2c457a9463ff6a0369a8ca83d45e26859aa695c
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-04-26 20:31:43 -0700

    more complete "lane" query now that SUL's records are present

--- src/main/java/edu/stanford/irt/eresources/LaneUpdate.java
+++ src/main/java/edu/stanford/irt/eresources/LaneUpdate.java
@@ -2,9 +2,12 @@ package edu.stanford.irt.eresources;
 
 public class LaneUpdate extends SolrLoader {
 
+    private static final String BASE_QUERY = "(recordType:auth OR recordType:bib OR "
+            + " recordType:class OR recordType:laneblog OR recordType:web)";
+
     @Override
     public void load() {
-        this.setUpdatedDateQuery("NOT recordType:pubmed");
+        this.setUpdatedDateQuery(BASE_QUERY);
         super.load();
     }
 }
