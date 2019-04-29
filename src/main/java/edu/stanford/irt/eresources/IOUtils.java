612aa503eb57bc6ae08768ffb2046ca189a46e09
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-04-29 09:49:28 -0700

    remove unused method

--- src/main/java/edu/stanford/irt/eresources/IOUtils.java
+++ src/main/java/edu/stanford/irt/eresources/IOUtils.java
@@ -1,10 +1,8 @@
 package edu.stanford.irt.eresources;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -18,21 +16,6 @@ public final class IOUtils {
 
     private IOUtils() {
         // private empty constructor
-    }
-
-    public static String getResourceAsString(final Class<?> clazz, final String name) {
-        StringBuilder sb = new StringBuilder();
-        try (InputStream input = clazz.getResourceAsStream(name);
-                InputStreamReader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
-                BufferedReader b = new BufferedReader(reader)) {
-            String s;
-            while ((s = b.readLine()) != null) {
-                sb.append(s).append('\n');
-            }
-        } catch (IOException e) {
-            throw new EresourceDatabaseException(e);
-        }
-        return sb.toString();
     }
 
     public static InputStream getStream(final URL url) throws IOException {
