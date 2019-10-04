ed8bd3d22c34a9b4594910b7beb2e285723208d9
Ryan Max Steinberg <ryanmax@stanford.edu>
2019-10-04 16:43:42 -0700

    new unit test

---
+++ src/test/java/edu/stanford/irt/eresources/sax/SAXEresourceTest.java
@@ -0,0 +1,86 @@
+package edu.stanford.irt.eresources.sax;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDateTime;
+
+import org.junit.Test;
+
+public class SAXEresourceTest {
+
+    @Test
+    public final void testSaxEresource() {
+        SAXEresource eresource = new SAXEresource();
+        SAXVersion version = new SAXVersion();
+        eresource.addAbbreviatedTitle("abtitle");
+        eresource.addAlternativeTitle("altitle");
+        eresource.addBroadMeshTerm("broadMeshTerm");
+        eresource.addMeshTerm("meshTerm");
+        eresource.addPublicationAuthor("author");
+        eresource.addPublicationAuthorFacetable("author");
+        eresource.addPublicationLanguage("publicationLanguage");
+        eresource.addPublicationType("publicationType");
+        eresource.addType("type");
+        eresource.addVersion(version);
+        eresource.setDate("");
+        eresource.setDescription("description");
+        eresource.setId("id");
+        eresource.setIsCore(true);
+        eresource.setIsDigital(true);
+        eresource.setIsLaneConnex(true);
+        eresource.setItemCount(new int[2]);
+        eresource.setKeywords("keywords");
+        eresource.setPrimaryType("type");
+        eresource.setPublicationAuthorsText(null);
+        eresource.setPublicationDate("date");
+        eresource.setPublicationIssue("publicationIssue");
+        eresource.setPublicationPages("pages");
+        eresource.setPublicationText(null);
+        eresource.setPublicationTitle("publicationTitle");
+        eresource.setPublicationVolume("publicationVolume");
+        eresource.setRecordId(123);
+        eresource.setRecordType("recordType");
+        eresource.setShortTitle("shortTitle");
+        eresource.setSortTitle("sortTitle");
+        eresource.setTitle("title");
+        eresource.setUpdated(LocalDateTime.now());
+        eresource.setYear(2000);
+        assertNotNull(eresource.toString());
+        assertNotNull(eresource.getAbbreviatedTitles());
+        assertNotNull(eresource.getAlternativeTitles());
+        assertNotNull(eresource.getBroadMeshTerms());
+        assertNotNull(eresource.getDate());
+        assertNotNull(eresource.getDescription());
+        assertNotNull(eresource.getId());
+        assertNotNull(eresource.getItemCount());
+        assertNotNull(eresource.getKeywords());
+        assertNotNull(eresource.getMeshTerms());
+        assertNotNull(eresource.getPrimaryType());
+        assertNotNull(eresource.getPublicationAuthors());
+        assertNotNull(eresource.getPublicationAuthorsFacetable());
+        assertNotNull(eresource.getPublicationAuthorsText());
+        assertNotNull(eresource.getPublicationDate());
+        assertNotNull(eresource.getPublicationIssue());
+        assertNotNull(eresource.getPublicationLanguages());
+        assertNotNull(eresource.getPublicationPages());
+        assertNotNull(eresource.getPublicationText());
+        assertNotNull(eresource.getPublicationTitle());
+        assertNotNull(eresource.getPublicationTypes());
+        assertNotNull(eresource.getPublicationVolume());
+        assertNotNull(eresource.getRecordId());
+        assertNotNull(eresource.getRecordType());
+        assertNotNull(eresource.getShortTitle());
+        assertNotNull(eresource.getSortTitle());
+        assertNotNull(eresource.getTitle());
+        assertNotNull(eresource.getTypes());
+        assertNotNull(eresource.getUpdated());
+        assertNotNull(eresource.getVersions());
+        assertNotNull(eresource.getYear());
+        assertTrue(eresource.isCore());
+        assertTrue(eresource.isDigital());
+        assertFalse(eresource.isEnglish());
+        assertTrue(eresource.isLaneConnex());
+    }
+}
