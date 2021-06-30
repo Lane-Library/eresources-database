45c07634227f81d94400c13b7c3104760f0b6b85
Ryan Max Steinberg <ryanmax@stanford.edu>
2021-06-30 16:39:48 -0700

    fix unit test

--- src/main/java/edu/stanford/irt/eresources/sax/LibGuideEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/sax/LibGuideEresourceProcessor.java
@@ -175,7 +175,7 @@ public class LibGuideEresourceProcessor extends AbstractEresourceProcessor {
             parser.parse(source);
             parser.setErrorHandler(this.errorHandler);
             Document doc = parser.getDocument();
-            NodeList guideAnchors = (NodeList) this.xpath.evaluate("//div[@id='s-lg-guide-tabs']/ul/li/a[starts-with(@href,'http')]", doc,
+            NodeList guideAnchors = (NodeList) this.xpath.evaluate("//div[@id='s-lg-guide-tabs']/ul/li/a[starts-with(@href,'http') or starts-with(@href,'file')]", doc,
                     XPathConstants.NODESET);
             for (int i = 0; i < guideAnchors.getLength(); i++) {
                 Element guideAnchor = (Element) guideAnchors.item(i);
