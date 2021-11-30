900f79256106d83bd74bf217176ee48eed85b3bb
Ryan Max Steinberg <ryanmax@stanford.edu>
2021-11-30 09:32:38 -0800

    Rename this variable to not match a restricted identifier.

--- src/main/java/edu/stanford/irt/eresources/marc/SulTypeFactoryHelper.java
+++ src/main/java/edu/stanford/irt/eresources/marc/SulTypeFactoryHelper.java
@@ -23,34 +23,34 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
 
     private static final String FORTY_ZEROES = StringUtils.repeat("0", 40);
 
-    public static Set<String> getTypes(final Record record) {
-        Set<String> types = pass1(record);
-        types.addAll(pass1(record));
-        types = pass2(record, new HashSet<>(types));
-        types = pass3(record, new HashSet<>(types));
-        types.addAll(pass4(record));
-        types.addAll(pass5(record));
-        types.addAll(pass6(record));
-        types.addAll(pass7(record));
-        types = pass8(record, new HashSet<>(types));
-        types.addAll(pass9(record));
-        types = passs10(record, new HashSet<>(types));
-        types = passs11(record, new HashSet<>(types));
-        types = passs12(record, new HashSet<>(types));
+    public static Set<String> getTypes(final Record marcRecord) {
+        Set<String> types = pass1(marcRecord);
+        types.addAll(pass1(marcRecord));
+        types = pass2(marcRecord, new HashSet<>(types));
+        types = pass3(marcRecord, new HashSet<>(types));
+        types.addAll(pass4(marcRecord));
+        types.addAll(pass5(marcRecord));
+        types.addAll(pass6(marcRecord));
+        types.addAll(pass7(marcRecord));
+        types = pass8(marcRecord, new HashSet<>(types));
+        types.addAll(pass9(marcRecord));
+        types = passs10(marcRecord, new HashSet<>(types));
+        types = passs11(marcRecord, new HashSet<>(types));
+        types = passs12(marcRecord, new HashSet<>(types));
         return types;
     }
 
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1401
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass1(final Record record) {
+    private static Set<String> pass1(final Record marcRecord) {
         Set<String> mytypes = new HashSet<>();
-        byte leaderByte6 = record.getLeaderByte(6);
-        byte leaderByte7 = record.getLeaderByte(7);
-        String f008 = getFields(record, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
+        byte leaderByte6 = marcRecord.getLeaderByte(6);
+        byte leaderByte7 = marcRecord.getLeaderByte(7);
+        String f008 = getFields(marcRecord, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
         if ('a' == leaderByte6 || 't' == leaderByte6) {
             if ('a' == leaderByte7 || 'm' == leaderByte7) {
                 mytypes.add("Book");
@@ -98,18 +98,18 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1452
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> pass2(final Record record, final Set<String> currentTypes) {
+    private static Set<String> pass2(final Record marcRecord, final Set<String> currentTypes) {
         if (!currentTypes.isEmpty()) {
             return currentTypes;
         }
-        byte leaderByte7 = record.getLeaderByte(7);
-        String f006 = getFields(record, "006").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
-        String f008 = getFields(record, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
+        byte leaderByte7 = marcRecord.getLeaderByte(7);
+        String f006 = getFields(marcRecord, "006").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
+        String f008 = getFields(marcRecord, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
         char f008Byte21 = f008.charAt(21);
         Set<String> mytypes = new HashSet<>();
         if ('s' == leaderByte7) {
@@ -147,17 +147,17 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1488
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> pass3(final Record record, final Set<String> currentTypes) {
+    private static Set<String> pass3(final Record marcRecord, final Set<String> currentTypes) {
         if (!currentTypes.isEmpty()) {
             return currentTypes;
         }
-        byte leaderByte7 = record.getLeaderByte(7);
-        String f008 = getFields(record, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
+        byte leaderByte7 = marcRecord.getLeaderByte(7);
+        String f008 = getFields(marcRecord, "008").map(Field::getData).findFirst().orElse(FORTY_ZEROES);
         char f008Byte21 = f008.charAt(21);
         Set<String> mytypes = new HashSet<>();
         if ('i' == leaderByte7) {
@@ -177,12 +177,12 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1508
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass4(final Record record) {
+    private static Set<String> pass4(final Record marcRecord) {
         Set<String> mytypes = new HashSet<>();
-        if (getSubfieldData(record, "999", "t").collect(Collectors.toSet()).contains("DATABASE")) {
+        if (getSubfieldData(marcRecord, "999", "t").collect(Collectors.toSet()).contains("DATABASE")) {
             mytypes.add("Database");
         }
         return mytypes;
@@ -192,24 +192,24 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1514
      * skipping this method as it depends on access_facet and does not seem worth replicating here
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass5(final Record record) {
+    private static Set<String> pass5(final Record marcRecord) {
         return Collections.emptySet();
     }
 
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1522
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass6(final Record record) {
+    private static Set<String> pass6(final Record marcRecord) {
         Pattern subAPattern = Pattern.compile(
                 "^(A\\d|F\\d|M\\d|MISC \\d|(MSS (CODEX|MEDIA|PHOTO|PRINTS))|PC\\d|SC[\\d|D|M]|V\\d)",
                 Pattern.CASE_INSENSITIVE);
-        for (Field f : getFields(record, "999").collect(Collectors.toList())) {
+        for (Field f : getFields(marcRecord, "999").collect(Collectors.toList())) {
             boolean mSpecColl = f.getSubfields().stream()
                     .anyMatch((final Subfield s) -> s.getCode() == 'm' && "SPEC-COLL".equals(s.getData()));
             boolean wAlphanum = f.getSubfields().stream()
@@ -226,13 +226,13 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1537
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass7(final Record record) {
-        String f245h = getSubfieldData(record, "245", "h").collect(Collectors.joining(" "));
+    private static Set<String> pass7(final Record marcRecord) {
+        String f245h = getSubfieldData(marcRecord, "245", "h").collect(Collectors.joining(" "));
         if (f245h.contains("manuscript")
-                && getSubfieldData(record, "999", "m").collect(Collectors.toSet()).contains("LANE-MED")) {
+                && getSubfieldData(marcRecord, "999", "m").collect(Collectors.toSet()).contains("LANE-MED")) {
             return Collections.singleton("Book");
         }
         return Collections.emptySet();
@@ -241,16 +241,16 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1550
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> pass8(final Record record, final Set<String> currentTypes) {
-        byte leaderByte6 = record.getLeaderByte(6);
-        byte leaderByte7 = record.getLeaderByte(7);
+    private static Set<String> pass8(final Record marcRecord, final Set<String> currentTypes) {
+        byte leaderByte6 = marcRecord.getLeaderByte(6);
+        byte leaderByte7 = marcRecord.getLeaderByte(7);
         if ("at".indexOf(leaderByte6) > -1 && "cd".indexOf(leaderByte7) > -1
-                && getSubfieldData(record, "999", "m").collect(Collectors.toSet()).contains("LANE-MED")) {
+                && getSubfieldData(marcRecord, "999", "m").collect(Collectors.toSet()).contains("LANE-MED")) {
             currentTypes.remove("Archive/Manuscript");
             currentTypes.add("Book");
         }
@@ -260,11 +260,11 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1561
      *
-     * @param record
+     * @param marcRecord
      * @return set of types
      */
-    private static Set<String> pass9(final Record record) {
-        if (getSubfieldData(record, "590", "a").anyMatch((final String s) -> s.contains("MARCit brief record"))) {
+    private static Set<String> pass9(final Record marcRecord) {
+        if (getSubfieldData(marcRecord, "590", "a").anyMatch((final String s) -> s.contains("MARCit brief record"))) {
             return Collections.singleton("Journal/Periodical");
         }
         return Collections.emptySet();
@@ -273,13 +273,13 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1570
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> passs10(final Record record, final Set<String> currentTypes) {
-        if (getSubfieldData(record, "914", "a").collect(Collectors.toSet()).contains("EQUIP")) {
+    private static Set<String> passs10(final Record marcRecord, final Set<String> currentTypes) {
+        if (getSubfieldData(marcRecord, "914", "a").collect(Collectors.toSet()).contains("EQUIP")) {
             return Collections.singleton("Equipment");
         }
         return currentTypes;
@@ -288,14 +288,14 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1581
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> passs11(final Record record, final Set<String> currentTypes) {
+    private static Set<String> passs11(final Record marcRecord, final Set<String> currentTypes) {
         if (currentTypes.isEmpty() || currentTypes.contains("Other")) {
-            String f245h = getSubfieldData(record, "245", "h").map((final String s) -> s.toLowerCase(Locale.US))
+            String f245h = getSubfieldData(marcRecord, "245", "h").map((final String s) -> s.toLowerCase(Locale.US))
                     .collect(Collectors.joining(" "));
             String format = null;
             if (Pattern.compile("(video|motion picture|filmstrip|vcd-dvd)").matcher(f245h).find()) {
@@ -309,7 +309,7 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
                     .matcher(f245h).find()) {
                 format = "Image";
             } else if (Pattern.compile("kit").matcher(f245h).find()) {
-                char f007Byte0 = getFields(record, "007").map(Field::getData).findFirst().orElse("0").charAt(0);
+                char f007Byte0 = getFields(marcRecord, "007").map(Field::getData).findFirst().orElse("0").charAt(0);
                 if ("ad".indexOf(f007Byte0) > -1) {
                     format = "Map";
                 } else if ('c' == f007Byte0) {
@@ -336,12 +336,12 @@ public final class SulTypeFactoryHelper extends MARCRecordSupport {
     /**
      * https://github.com/sul-dlss/searchworks_traject_indexer/blob/master/lib/traject/config/sirsi_config.rb#L1622
      *
-     * @param record
+     * @param marcRecord
      * @param currentTypes
      *            set of assigned types
      * @return set of types
      */
-    private static Set<String> passs12(final Record record, final Set<String> currentTypes) {
+    private static Set<String> passs12(final Record marcRecord, final Set<String> currentTypes) {
         if (currentTypes.isEmpty()) {
             return Collections.emptySet();
         }
