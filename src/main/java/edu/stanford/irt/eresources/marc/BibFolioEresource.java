7aa14dcf59aaf2ba4529ab9bc88cae4d634792e9
Ryan Max Steinberg <ryanmax@stanford.edu>
2022-09-08 08:52:09 -0700

    missed in previous commit

---
+++ src/main/java/edu/stanford/irt/eresources/marc/BibFolioEresource.java
@@ -0,0 +1,217 @@
+package edu.stanford.irt.eresources.marc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+import edu.stanford.irt.eresources.Eresource;
+import edu.stanford.irt.eresources.LanguageMap;
+import edu.stanford.irt.eresources.Version;
+import edu.stanford.irt.eresources.VersionComparator;
+import edu.stanford.lane.catalog.FolioRecord;
+
+/**
+ * An Eresource that encapsulates the Folio Records from which it is derived.
+ */
+public class BibFolioEresource implements Eresource {
+
+    private static final Comparator<Version> COMPARATOR = new VersionComparator();
+
+    private static final LanguageMap LANGUAGE_MAP = new LanguageMap();
+
+    private FolioRecord folioRecord;
+
+    private List<Version> versions;
+
+    protected HTTPLaneLocationsService locationsService;
+
+    public BibFolioEresource(final FolioRecord folioRecord, final HTTPLaneLocationsService locationsService) {
+        this.folioRecord = folioRecord;
+        this.locationsService = locationsService;
+    }
+
+    @Override
+    public Collection<String> getAbbreviatedTitles() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Collection<String> getAlternativeTitles() {
+        return this.folioRecord.jsonContext().read("$.instance.alternativeTitles.*.alternativeTitle");
+    }
+
+    @Override
+    public Collection<String> getBroadMeshTerms() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getDate() {
+        return this.folioRecord.jsonContext().read("$.instance.publication[0].dateOfPublication");
+    }
+
+    @Override
+    public String getDescription() {
+        List<String> notes = this.folioRecord.jsonContext().read("$.instance.notes.[?(@.staffOnly=='false')].*note");
+        return notes.stream().collect(Collectors.joining(" "));
+    }
+
+    @Override
+    public String getId() {
+        return this.folioRecord.jsonContext().read("$.instance.hrid");
+    }
+
+    @Override
+    public String getKeywords() {
+        // too much data?
+        // problem that it ignores both reserves and authority augmentables?
+        return this.folioRecord.toString();
+    }
+
+    @Override
+    public Collection<String> getMeshTerms() {
+        return this.folioRecord.jsonContext().read("$.instance.subjects.*");
+    }
+
+    @Override
+    public String getPrimaryType() {
+        // not sure where to get this from instance record
+        return null;
+    }
+
+    @Override
+    public Collection<String> getPublicationAuthors() {
+        return this.folioRecord.jsonContext().read("$.instance.contributors.*name");
+    }
+
+    @Override
+    public String getPublicationAuthorsText() {
+        return this.getPublicationAuthors().stream().collect(Collectors.joining("; "));
+    }
+
+    @Override
+    public String getPublicationDate() {
+        return null;
+    }
+
+    @Override
+    public String getPublicationIssue() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getPublicationLanguages() {
+        List<String> languages = this.folioRecord.jsonContext().read("$.instance.languages.*");
+        return languages.stream().map(String::toLowerCase).map(LANGUAGE_MAP::getLanguage).collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPublicationPages() {
+        return null;
+    }
+
+    @Override
+    public String getPublicationText() {
+        return null;
+    }
+
+    @Override
+    public String getPublicationTitle() {
+        return null;
+    }
+
+    @Override
+    public Collection<String> getPublicationTypes() {
+        return Collections.emptySet();
+    }
+
+    @Override
+    public String getPublicationVolume() {
+        return null;
+    }
+
+    @Override
+    public int getRecordId() {
+        // TODO: determine strategy around ID handling
+        String hrid = this.folioRecord.jsonContext().read("$.instance.hrid", String.class).replaceAll("[^\\d]", "");
+        return Integer.parseInt(hrid);
+    }
+
+    @Override
+    public String getRecordType() {
+        return "bib";
+    }
+
+    @Override
+    public String getShortTitle() {
+        // no 149 ^a equivalent but returning null could mess up relevance
+        return this.getSortTitle();
+    }
+
+    @Override
+    public String getSortTitle() {
+        return this.folioRecord.jsonContext().read("$.instance.indexTitle");
+    }
+
+    @Override
+    public String getTitle() {
+        return this.folioRecord.jsonContext().read("$.instance.title");
+    }
+
+    @Override
+    public Collection<String> getTypes() {
+        // not sure where to get this from Folio instance records
+        return Collections.emptyList();
+    }
+
+    @Override
+    public LocalDateTime getUpdated() {
+        // TODO is this even used?
+        return null;
+    }
+
+    @Override
+    public List<Version> getVersions() {
+        if (this.versions == null) {
+            Collection<Version> versionSet = new TreeSet<>(COMPARATOR);
+            for (Map<String, ?> holding : this.folioRecord.getHoldings()) {
+                Version version = new FolioVersion(holding, this, this.locationsService);
+                if (!version.getLinks().isEmpty()) {
+                    versionSet.add(version);
+                }
+            }
+            this.versions = Collections.unmodifiableList(new ArrayList<>(versionSet));
+        }
+        return new ArrayList<>(this.versions);
+    }
+
+    @Override
+    public int getYear() {
+        String date = this.getDate();
+        if (null != date && date.length() >= 4) {
+            return Integer.parseInt(date.substring(0, 4));
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean isCore() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnglish() {
+        return this.getPublicationLanguages().contains("English");
+    }
+
+    @Override
+    public boolean isLaneConnex() {
+        return this.getMeshTerms().contains("LaneConnex");
+    }
+}
