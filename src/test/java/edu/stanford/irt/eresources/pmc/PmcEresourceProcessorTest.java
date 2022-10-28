754c51e96d0a53a5ca8bac25ae1c96883289a649
Ryan Max Steinberg <ryanmax@stanford.edu>
2022-10-27 22:47:28 -0700

    more test coverage

--- src/test/java/edu/stanford/irt/eresources/pmc/PmcEresourceProcessorTest.java
+++ src/test/java/edu/stanford/irt/eresources/pmc/PmcEresourceProcessorTest.java
@@ -11,6 +11,7 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.Locator;
 import org.xml.sax.helpers.AttributesImpl;
 
+import edu.stanford.irt.eresources.EresourceDatabaseException;
 import edu.stanford.irt.eresources.marc.LaneDedupAugmentation;
 import net.sf.saxon.tree.util.AttributeCollectionImpl;
 
@@ -33,6 +34,27 @@ public class PmcEresourceProcessorTest {
                 PmcEresourceProcessorTest.class.getResource("efetch.xml").toExternalForm(),
                 PmcEresourceProcessorTest.class.getResource("jlist.csv").toExternalForm(), this.contentHandler,
                 this.laneDedupAugmentation, "key");
+    }
+
+    @Test
+    public final void testBadUrl() throws Exception {
+        this.processor = new PmcEresourceProcessor("file://",
+                PmcEresourceProcessorTest.class.getResource("jlist.csv").toExternalForm(), this.contentHandler,
+                this.laneDedupAugmentation, "key");
+        this.thrown.expect(EresourceDatabaseException.class);
+        this.thrown.expectMessage("java.io.FileNotFoundException:  (No such file or directory)");
+        this.contentHandler.startDocument();
+        EasyMock.expectLastCall().atLeastOnce();
+        this.contentHandler.startElement(isA(String.class), isA(String.class), isA(String.class),
+                isA(AttributesImpl.class));
+        EasyMock.expectLastCall().atLeastOnce();
+        this.contentHandler.setDocumentLocator(isA(Locator.class));
+        EasyMock.expectLastCall();
+        EasyMock.expect(this.laneDedupAugmentation.isDuplicate("dnlm", "101565857")).andReturn(false);
+        EasyMock.expect(this.laneDedupAugmentation.isDuplicate("issn", "2190572x")).andReturn(false);
+        EasyMock.expect(this.laneDedupAugmentation.isDuplicate("issn", "21905738")).andReturn(false);
+        EasyMock.replay(this.contentHandler, this.laneDedupAugmentation);
+        this.processor.process();
     }
 
     @Test
