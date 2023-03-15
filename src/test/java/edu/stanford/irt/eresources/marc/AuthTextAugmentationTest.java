db825ded0c80e78f73b7cc269b3fcbccb52a2a17
Ryan Max Steinberg <ryanmax@stanford.edu>
2023-03-15 13:57:18 -0700

    new unit test

---
+++ src/test/java/edu/stanford/irt/eresources/marc/AuthTextAugmentationTest.java
@@ -0,0 +1,46 @@
+package edu.stanford.irt.eresources.marc;
+
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.mock;
+import static org.easymock.EasyMock.replay;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AuthTextAugmentationTest {
+
+    private AuthTextAugmentation augmentation;
+
+    private Map<String, String> augmentations;
+
+    private String objectFile = "/tmp/auth-objects";
+
+    private AugmentationsService service;
+
+    @Before
+    public void setUp() throws Exception {
+        this.service = mock(AugmentationsService.class);
+        this.augmentations = Collections.singletonMap("1", "variant authority text");
+        expect(this.service.buildAugmentations()).andReturn(this.augmentations);
+        replay(this.service);
+        this.augmentation = new AuthTextAugmentation(this.objectFile, this.service);
+        verify(this.service);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        new File(this.objectFile).delete();
+    }
+
+    @Test
+    public final void testGetAugmentations() {
+        assertEquals("variant authority text", this.augmentation.getAuthAugmentations("1"));
+    }
+}
