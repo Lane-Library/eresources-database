ec6737af5b0315404201762d587cdf92ad4c4c4a
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-05-20 12:08:14 -0700

    tidy comments; also remove isLane check as this is done in MetaDB inclusion query

--- src/main/java/edu/stanford/irt/eresources/marc/sul/SulMARCRecordEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/marc/sul/SulMARCRecordEresourceProcessor.java
@@ -71,9 +71,10 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
             if (null == marcRecord) {
                 log.info("dropping non-marc record: {}", folioRecord);
             }
-            //TODO: remove all but deduplication check now that we rely on MetaDB? what about keyword check?
-            if (null != marcRecord && isInScope(marcRecord) && !isLane(folioRecord, marcRecord)
-                    && !isLaneDuplicate(marcRecord)) {
+            // retain inclusion checks here b/c MetaDB inclusion
+            // (catalog-service getSulUpdates.sql) does not include strategies
+            // like digital book keywords and fiction
+            if (null != marcRecord && isInScope(marcRecord) && !isLaneDuplicate(marcRecord)) {
                 this.eresourceHandler
                         .handleEresource(new SulMarcEresource(marcRecord, this.keywordsStrategy, this.lcshMapManager));
             }
@@ -84,19 +85,9 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
         return this.inclusionStrategies.stream().anyMatch((final InclusionStrategy is) -> is.isAcceptable(marcRecord));
     }
 
-    private boolean isLane(final FolioRecord folioRecord, final Record marcRecord) {
-        //FIXME: this will never evaluate to true since SUL records lack items/holdings data
-        if (folioRecord.toString().contains("\"libraryName\": \"Lane")) {
-            return true;
-        } else if (MARCRecordSupport.getRecordId(marcRecord).startsWith("L")) {
-            log.info("Lane HRID but not Lane library: {}", folioRecord);
-            return true;
-        }
-        return false;
-    }
-
     private boolean isLaneDuplicate(final Record marcRecord) {
-        // LANECAT-776, LANECAT-872: presence of a 909 in SUL records triggers inclusion and skips deduplication
+        // LANECAT-776, LANECAT-872: presence of a 909 in SUL records triggers
+        // inclusion and skips deduplication
         if (MARCRecordSupport.getFields(marcRecord, "909").count() > 0) {
             return false;
         }
