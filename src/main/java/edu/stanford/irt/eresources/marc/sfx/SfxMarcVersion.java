cae8b18011ea91b34315f14b678f14d17e72a245
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-08-08 10:35:22 -0700

    clarify comment and remove commented code

--- src/main/java/edu/stanford/irt/eresources/marc/sfx/SfxMarcVersion.java
+++ src/main/java/edu/stanford/irt/eresources/marc/sfx/SfxMarcVersion.java
@@ -18,18 +18,11 @@ public class SfxMarcVersion extends SulMarcVersion {
 
     @Override
     // SFX export puts object ID in 866 ^z and we don't want that to show in UI
+    // SFX 866 ^x contains target name like "Elsevier SD ScienceDirect Available
+    // Journals:Full Text"
+    // neither is helpful for display
     public String getAdditionalText() {
-        String additionalText = null;
-        // might consider using note info from SFX, assuming it's user-friendly
-        // List<Field> fields = getFields(this.bib, "866").toList();
-        // if (fields.size() > 1) {
-        // additionalText = "";
-        // } else if (fields.size() == 1) {
-        // additionalText = fields.get(0).getSubfields().stream().filter((final
-        // Subfield s) -> 'z' == s.getCode())
-        // .map(Subfield::getData).findFirst().orElse(null);
-        // }
-        return additionalText;
+        return null;
     }
 
     @Override
