b27f03c1fdf672ccbfe268dadb9170909880a079
Ryan Max Steinberg <ryanmax@stanford.edu>
2024-09-16 15:53:36 -0700

    convert ISBN-10 to ISBN-13 for dup checks
    related to LANEWEB-11364
    also, clarify a little by not extending SulMARCRecordEresourceProcessor

--- src/main/java/edu/stanford/irt/eresources/marc/sfx/SfxRecordEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/marc/sfx/SfxRecordEresourceProcessor.java
@@ -1,20 +1,23 @@
 package edu.stanford.irt.eresources.marc.sfx;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.apache.commons.validator.routines.ISBNValidator;
+
+import edu.stanford.irt.eresources.AbstractEresourceProcessor;
 import edu.stanford.irt.eresources.EresourceHandler;
 import edu.stanford.irt.eresources.marc.KeywordsStrategy;
 import edu.stanford.irt.eresources.marc.LaneDedupAugmentation;
 import edu.stanford.irt.eresources.marc.MARCRecordSupport;
 import edu.stanford.irt.eresources.marc.RecordCollectionFactory;
-import edu.stanford.irt.eresources.marc.sul.InclusionStrategy;
-import edu.stanford.irt.eresources.marc.sul.SulMARCRecordEresourceProcessor;
-import edu.stanford.irt.eresources.pmc.PmcDedupAugmentation;
 import edu.stanford.lane.catalog.Record;
 import edu.stanford.lane.catalog.RecordCollection;
+import edu.stanford.lane.catalog.TextHelper;
 import edu.stanford.lane.lcsh.LcshMapManager;
 
-public class SfxRecordEresourceProcessor extends SulMARCRecordEresourceProcessor {
+public class SfxRecordEresourceProcessor extends AbstractEresourceProcessor {
 
     private EresourceHandler eresourceHandler;
 
@@ -24,14 +27,14 @@ public class SfxRecordEresourceProcessor extends SulMARCRecordEresourceProcessor
 
     private RecordCollectionFactory recordCollectionFactory;
 
+    private LaneDedupAugmentation laneDedupAugmentation;
+
     public SfxRecordEresourceProcessor(final EresourceHandler eresourceHandler, final KeywordsStrategy keywordsStrategy,
-            final RecordCollectionFactory recordCollectionFactory, final LaneDedupAugmentation laneDedupAugmentation,
-            final PmcDedupAugmentation pmcDedupAugmentation, final List<InclusionStrategy> inclusionStrategies) {
-        super(eresourceHandler, keywordsStrategy, recordCollectionFactory, laneDedupAugmentation, pmcDedupAugmentation,
-                inclusionStrategies);
+            final RecordCollectionFactory recordCollectionFactory, final LaneDedupAugmentation laneDedupAugmentation) {
         this.recordCollectionFactory = recordCollectionFactory;
         this.eresourceHandler = eresourceHandler;
         this.keywordsStrategy = keywordsStrategy;
+        this.laneDedupAugmentation = laneDedupAugmentation;
     }
 
     @Override
@@ -49,4 +52,23 @@ public class SfxRecordEresourceProcessor extends SulMARCRecordEresourceProcessor
     private boolean hasIsbn(final Record marcRecord) {
         return MARCRecordSupport.getFields(marcRecord, "020").count() > 0;
     }
+
+    private boolean isDuplicate(final Record marcRecord) {
+        Set<String> keys = new HashSet<>();
+        for (String isbn : MARCRecordSupport.getSubfieldData(marcRecord, "020", "a").map(String::trim)
+                .map(TextHelper::cleanIsxn).filter((final String s) -> !s.isEmpty()).collect(Collectors.toSet())) {
+            keys.add(LaneDedupAugmentation.KEY_ISBN + LaneDedupAugmentation.SEPARATOR + isbn);
+            if (isbn.length() == 10) {
+                keys.add(LaneDedupAugmentation.KEY_ISBN + LaneDedupAugmentation.SEPARATOR
+                        + ISBNValidator.getInstance().convertToISBN13(isbn));
+            }
+        }
+        for (String entry : keys) {
+            if (this.laneDedupAugmentation.isDuplicate(entry)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/edu/stanford/irt/eresources/marc/sul/SulMARCRecordEresourceProcessor.java
+++ src/main/java/edu/stanford/irt/eresources/marc/sul/SulMARCRecordEresourceProcessor.java
@@ -81,11 +81,11 @@ public class SulMARCRecordEresourceProcessor extends AbstractEresourceProcessor 
         }
     }
 
-    public boolean isInScope(final Record marcRecord) {
+    private boolean isInScope(final Record marcRecord) {
         return this.inclusionStrategies.stream().anyMatch((final InclusionStrategy is) -> is.isAcceptable(marcRecord));
     }
 
-    public boolean isDuplicate(final Record marcRecord) {
+    private boolean isDuplicate(final Record marcRecord) {
         // LANECAT-776, LANECAT-872: presence of a 909 in SUL records triggers
         // inclusion and skips deduplication
         if (MARCRecordSupport.getFields(marcRecord, "909").count() > 0) {
